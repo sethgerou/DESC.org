b5ce00b3f0471f6ef22cb851c5485c0439f4bbe8
Austin Kading & Seth Gerou <pair+austin-kading.sethgerou@devbootcamp.com>
2017-05-22 16:29:33 -0700

    config change to serve static assets

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,5 +21,6 @@ module Desc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
   end
 end
