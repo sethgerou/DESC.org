4ec93e5de1a77cc22315d534b19280f935766e17
Seth Gerou <seth.gerou@gmail.com>
2017-06-06 10:51:13 -0700

    edit asset setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
 config.action_mailer.default_url_options = { host: 'descorg.herokuapp.com' }
 
